a6ed371cb3efc6dcc1390ec8321e560ac4554b3c
Tanour <88142279+Tanour@users.noreply.github.com>
2022-04-14 18:29:41 +0200

    Update config.lua

--- tShop/config.lua
+++ tShop/config.lua
@@ -20,24 +20,23 @@ Config = {
 },
 
     Shops = {
-        {pos = vector3(26.31, -1346.73, 29.5), pedPos = vector3(24.33, -1346.95, 28.5), heading = 262.04, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(373.875, 325.896, 103.566), pedPos = vector3(372.61, 326.9, 102.57), heading = 250.35, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(2557.458, 382.282, 108.622), pedPos = vector3(2557.04, 380.81, 107.62), heading = 350.72, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(-3038.939, 585.954, 7.908), pedPos = vector3(-3039.03, 584.38, 6.91), heading = 14.55, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(-3241.927, 1001.462, 12.830), pedPos = vector3(-3242.66, 999.83, 11.83), heading = 349.66, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(547.431, 2671.710, 42.156), pedPos = vector3(549.23, 2671.28, 41.16), heading = 109.50, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(1961.464, 3740.672, 32.343), pedPos = vector3(1960.01, 3740.21, 31.34), heading = 293.42, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(2678.916, 3280.671, 55.241), pedPos = vector3(2678.02, 3279.39, 54.24), heading = 325.27, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(1729.216, 6414.131, 35.037), pedPos = vector3(1727.82, 6415.23, 34.04), heading = 241.24, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(1135.808, -982.281, 46.415), pedPos = vector3(1133.92, -982.33, 45.42), heading = 273.98, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(-1222.915, -906.983, 12.326), pedPos = vector3(-1221.83, -908.56, 11.33), heading = 34.43, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(-1487.553, -379.107, 40.163), pedPos = vector3(-1486.32, -377.88, 39.16), heading = 132.27, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(1166.024, 2708.930, 38.157), pedPos = vector3(1165.78, 2711.08, 38.16), heading = 177.24, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(-2968.243, 390.910, 15.043), pedPos = vector3(-2966.12, 391.21, 14.04), heading = 79.1, pedModel = "mp_m_shopkeep_01"},
-        {pos = vector3(-707.85, -913.63, 19.22), pedPos = vector3(-706.01, -913.62, 18.22), heading = 87.84, pedModel = "mp_m_shopkeep_01"},
+        vector3(26.31, -1346.73, 29.5),
+        vector3(373.875, 325.896, 103.566),
+        vector3(2557.458, 382.282, 108.622),
+        vector3(-3038.939, 585.954, 7.908),
+        vector3(-3241.927, 1001.462, 12.830), 
+        vector3(547.431, 2671.710, 42.156), 
+        vector3(1961.464, 3740.672, 32.343),
+        vector3(2678.916, 3280.671, 55.241), 
+        vector3(1729.216, 6414.131, 35.037), 
+        vector3(1135.808, -982.281, 46.415), 
+        vector3(-1222.915, -906.983, 12.326), 
+        vector3(-1487.553, -379.107, 40.163), 
+        vector3(1166.024, 2708.930, 38.157), 
+        vector3(-2968.243, 390.910, 15.043), 
+        vector3(-707.85, -913.63, 19.22), 
 
-    },
-
+    }
 }
 
 
